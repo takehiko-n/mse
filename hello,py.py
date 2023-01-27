2e9aa3dd9fc5fae3b4779f6284a69fd1b48d6245
takehiko-n <32116219+takehiko-n@users.noreply.github.com>
2023-01-27 09:57:03 +0900

    Update hello,py.py

--- hello,py.py
+++ hello,py.py
@@ -1,4 +1,4 @@
 Python 3.10.6 (main, Nov 14 2022, 16:10:14) [GCC 11.3.0] on linux
 Type "help", "copyright", "credits" or "license()" for more information.
-print("Hello world")
+print("Hello world!")
 Hello world
